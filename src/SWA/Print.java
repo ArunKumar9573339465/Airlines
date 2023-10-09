b3da4815b699e7c420ddfb945a04cc20eaef24db
Arun Kumar <Arun Kumar@DESKTOP-D0PCNLH>
2023-10-09 14:54:45 +0530

    First Commit

---
+++ src/SWA/Print.java
@@ -0,0 +1,13 @@
+package SWA;
+
+public class Print {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+System.out.println("Printing hello ...");
+System.out.println("Printing second hello");
+
+	}
+
+}
